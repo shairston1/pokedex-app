c91a650b06e0909dd8f751556d568a935506f898
shairston1 <hairston531@gmail.com>
2023-04-20 16:18:10 -0400

    Added For loop

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 let pokemonList = [
 
   {
-      name: 'charizard',
+      name: 'Charizard',
       height: 1.7,
       types: ['fire', 'flying']
   },
   {
-      name: 'venusaur',
+      name: 'Venusaur',
       height: 2,
       types: ['grass', 'poison']
   },
   {
-      name: 'weedle',
+      name: 'Weedle',
       height: 0.3,
       types: ['bug', 'poison']
   },
 
 ];
+
+/* iterates through pokemon list array, first checks if height is greater or equal
+to 2, if condition is met a special message is printed with the name and height of pokemon
+*/
+for (let i=0; i < pokemonList.length; i++){
+  if (pokemonList[i].height >=2){
+  document.write(pokemonList[i].name + ' (height: ' +  pokemonList[i].height + ') - That is a big pokemon!</br>')}
+  else {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</br>')}
+  }
+  
